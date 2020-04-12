4d63b03f9eae3ff03d09bbfcd816ab7fa1f2fe0d
ShubhamVerma1811 <imshubhamverma.sv@gmail.com>
2020-04-12 14:43:59 +0530

    Updates

---
+++ precache-manifest.be2a71d90d34a31a8e099cbd7ff33766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c425d40302d411fe7d24cade63e25b9f",
+    "url": "/Movies-App-React/index.html"
+  },
+  {
+    "revision": "f242dcc4764180225728",
+    "url": "/Movies-App-React/static/css/main.0c414784.chunk.css"
+  },
+  {
+    "revision": "7b47a8a12c0c8104cabb",
+    "url": "/Movies-App-React/static/js/2.e09404d0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Movies-App-React/static/js/2.e09404d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f242dcc4764180225728",
+    "url": "/Movies-App-React/static/js/main.6cb2fc6a.chunk.js"
+  },
+  {
+    "revision": "2f05fe5860e184a907da",
+    "url": "/Movies-App-React/static/js/runtime-main.ae4b6522.js"
+  }
+]);
